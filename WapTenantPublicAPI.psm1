0844393ed19c3c8a26154fd40ab63c122ae3e8d4
Ben Newton <istairbn@users.noreply.github.com>
2017-05-23 11:16:05 +0100

    Fixed Network Type issue
    VMNetwork objects being passed as "Option" type rather than "Network" Type. 
    Matching on the name instead

--- WapTenantPublicAPI.psm1
+++ WapTenantPublicAPI.psm1
@@ -1757,7 +1757,9 @@ function New-WAPVMRoleParameterObject {
                 Add-Member -InputObject $Output -MemberType NoteProperty -Name $P.Name -Value 'domain\username:password' -Force
             } elseif ($P.Type -eq 'Network') {
                 Add-Member -InputObject $Output -MemberType NoteProperty -Name $P.Name -Value $($VMNetwork.Name) -Force
-            } elseif ($P.DefaultValue) {
+            } elseif ($P.Name -eq 'VMRoleNetworkRef') {
+                Add-Member -InputObject $Output -MemberType NoteProperty -Name $P.Name -Value $($VMNetwork.Name) -Force
+            }elseif ($P.DefaultValue) {
                 Add-Member -InputObject $Output -MemberType NoteProperty -Name $P.Name -Value $P.DefaultValue -Force
             } elseif ($Interactive) {
                 $result = Read-Host -Prompt "Enter a value for $($P.Name) of type $($P.Type)"
